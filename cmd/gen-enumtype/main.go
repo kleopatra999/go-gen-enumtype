f8f36fff583aab5c697ef32b1295b6b269b6b6c3
Peter Edge <peter.edge@gmail.com>
2015-03-24 00:05:13 +0100

    commit

--- cmd/gen-enumtype/main.go
+++ cmd/gen-enumtype/main.go
@@ -3,34 +3,23 @@ package main
 import (
 	"errors"
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
-	"text/template"
-	"unicode"
+
+	"github.com/peter-edge/go-gen-annotatedstruct"
+	"github.com/peter-edge/go-gen-common"
 )
 
 const (
-	genEnumtypeAnnotiation = "@gen-enumtype"
+	genEnumtypeAnnotation = "@gen-enumtype"
+	filePrefix            = "gen_enumtype_"
 )
 
 var (
-	ErrNil                     = errors.New("gen-enumtype: nil")
-	ErrEmpty                   = errors.New("gen-enumtype: empty")
-	ErrStringEmpty             = errors.New("gen-enumtype: string empty")
-	ErrGofileMustBeSet         = errors.New("gen-enumtype: $GOFILE must be set")
-	ErrCannotParseAnnotation   = errors.New("gen-enumtype: cannot parse annotation")
-	ErrExpectedOneSpec         = errors.New("gen-enumtype: expected one spec")
-	ErrExpectedTypeSpec        = errors.New("gen-enumtype: expected value spec")
-	ErrExpectedStructType      = errors.New("gen-enumtype: expected struct type")
-	ErrDuplicateAnnotation     = errors.New("gen-enumtype: duplicate annotation")
+	ErrInvalidAnnotationData   = errors.New("gen-enumtype: invalid annotation data")
 	ErrDuplicateAnnotationData = errors.New("gen-enumtype: duplicate annotation data")
-
-	debug = false
 )
 
 type GenData struct {
@@ -186,6 +175,7 @@ func templateString() string {
 	s += "{{end}}"
 	return s
 }
+
 func main() {
 	if err := generate(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
@@ -194,195 +184,44 @@ func main() {
 	os.Exit(0)
 }
 
-type annotation struct {
-	enumType  string
-	enumValue string
-	id        uint
-}
-
-func (this annotation) empty() bool {
-	return this.enumType == "" && this.enumValue == "" && this.id == 0
-}
-
 func generate() error {
-	goFile := os.Getenv("GOFILE")
-	if goFile == "" {
-		return ErrGofileMustBeSet
-	}
-	return generateFromEnv(goFile)
-}
-
-func generateFromEnv(goFile string) error {
-	mode := parser.ParseComments
-	if debug {
-		mode |= parser.Trace
-	}
-	astFile, err := parser.ParseFile(token.NewFileSet(), goFile, nil, mode)
+	parseResult, err := annotatedstruct.ParseFromGofile(genEnumtypeAnnotation)
 	if err != nil {
 		return err
 	}
-	return generateFromAstFile(goFile, astFile)
+	return generateFromParseResult(parseResult)
 }
 
-func generateFromAstFile(goFile string, astFile *ast.File) error {
-	pkg, err := packageFromAstFile(astFile)
+func generateFromParseResult(parseResult *annotatedstruct.ParseResult) error {
+	enumTypeToEnumValues, err := getEnumTypeToEnumValues(parseResult)
 	if err != nil {
 		return err
 	}
-	genDecls, err := genDeclsFromAstFile(astFile)
-	if err != nil {
-		return err
-	}
-	return generateFromGenDecls(goFile, pkg, genDecls)
+	return common.WriteFileFromTemplateString(filePrefix, parseResult.File, templateString(), getGenData(parseResult.Package, enumTypeToEnumValues))
 }
 
-func packageFromAstFile(astFile *ast.File) (string, error) {
-	if astFile.Name == nil {
-		return "", ErrNil
-	}
-	if astFile.Name.Name == "" {
-		return "", ErrStringEmpty
-	}
-	return astFile.Name.Name, nil
-}
-
-func genDeclsFromAstFile(astFile *ast.File) ([]*ast.GenDecl, error) {
-	if astFile.Decls == nil {
-		return nil, ErrNil
-	}
-	genDecls := make([]*ast.GenDecl, 0)
-	if astFile.Decls != nil {
-		for _, decl := range astFile.Decls {
-			if genDecl, ok := decl.(*ast.GenDecl); ok {
-				genDecls = append(genDecls, genDecl)
-			}
+func getEnumTypeToEnumValues(parseResult *annotatedstruct.ParseResult) (map[string][]*EnumValue, error) {
+	enumTypeToEnumValues := make(map[string][]*EnumValue)
+	for annotation, structDescriptor := range parseResult.AnnotationToStructDescriptor {
+		split := strings.Split(annotation, " ")
+		if len(split) != 4 {
+			return nil, ErrInvalidAnnotationData
 		}
-	}
-	return genDecls, nil
-}
-
-func generateFromGenDecls(goFile string, pkg string, genDecls []*ast.GenDecl) error {
-	annotatedGenDecls, err := getAnnotatedGenDecls(genDecls)
-	if err != nil {
-		return err
-	}
-	return generateFromAnnotatedGenDecls(goFile, pkg, annotatedGenDecls)
-}
-
-func getAnnotatedGenDecls(genDecls []*ast.GenDecl) (map[annotation]*ast.GenDecl, error) {
-	annotatedGenDecls := make(map[annotation]*ast.GenDecl)
-	for _, genDecl := range genDecls {
-		annotation, err := getGenDeclAnnotation(genDecl)
+		enumType := split[1]
+		enumValueName := split[2]
+		enumValueId, err := strconv.Atoi(split[3])
 		if err != nil {
 			return nil, err
 		}
-		if !annotation.empty() {
-			annotatedGenDecls[annotation] = genDecl
+		if _, ok := enumTypeToEnumValues[enumType]; !ok {
+			enumTypeToEnumValues[enumType] = make([]*EnumValue, 0)
 		}
-	}
-	return annotatedGenDecls, nil
-}
-
-func getGenDeclAnnotation(genDecl *ast.GenDecl) (annotation, error) {
-	if genDecl.Doc != nil && genDecl.Doc.List != nil && len(genDecl.Doc.List) > 0 {
-		for _, comment := range genDecl.Doc.List {
-			if index := strings.Index(comment.Text, genEnumtypeAnnotiation); index != -1 {
-				return parseAnnotation(comment.Text[index:])
-			}
-		}
-	}
-	return annotation{}, nil
-}
-
-func parseAnnotation(text string) (annotation, error) {
-	split := strings.Split(text, " ")
-	if len(split) != 4 {
-		return annotation{}, ErrCannotParseAnnotation
-	}
-	id, err := strconv.Atoi(split[3])
-	if err != nil {
-		return annotation{}, err
-	}
-	return annotation{split[1], split[2], uint(id)}, nil
-}
-
-func generateFromAnnotatedGenDecls(goFile string, pkg string, annotatedGenDecls map[annotation]*ast.GenDecl) error {
-	annotatedTypeSpecs, err := getAnnotatedTypeSpecs(annotatedGenDecls)
-	if err != nil {
-		return err
-	}
-	return generateFromAnnotatedTypeSpecs(goFile, pkg, annotatedTypeSpecs)
-}
-
-func getAnnotatedTypeSpecs(annotatedGenDecls map[annotation]*ast.GenDecl) (map[annotation]*ast.TypeSpec, error) {
-	annotatedTypeSpecs := make(map[annotation]*ast.TypeSpec)
-	for annotation, genDecl := range annotatedGenDecls {
-		if genDecl.Specs == nil {
-			return nil, ErrNil
-		}
-		if len(genDecl.Specs) != 1 {
-			return nil, ErrExpectedOneSpec
-		}
-		if typeSpec, ok := genDecl.Specs[0].(*ast.TypeSpec); ok {
-			annotatedTypeSpecs[annotation] = typeSpec
-		} else {
-			return nil, ErrExpectedTypeSpec
-		}
-	}
-	return annotatedTypeSpecs, nil
-}
-
-func generateFromAnnotatedTypeSpecs(goFile string, pkg string, annotatedTypeSpecs map[annotation]*ast.TypeSpec) error {
-	annotationToStructName, err := getAnnotationToStructName(annotatedTypeSpecs)
-	if err != nil {
-		return err
-	}
-	return generateFromAnnotationToStructName(goFile, pkg, annotationToStructName)
-}
-
-func getAnnotationToStructName(annotatedTypeSpecs map[annotation]*ast.TypeSpec) (map[annotation]string, error) {
-	annotationToStructName := make(map[annotation]string)
-	for annotation, typeSpec := range annotatedTypeSpecs {
-		if typeSpec.Type == nil {
-			return nil, ErrNil
-		}
-		if _, ok := typeSpec.Type.(*ast.StructType); !ok {
-			return nil, ErrExpectedStructType
-		}
-		if typeSpec.Name == nil {
-			return nil, ErrNil
-		}
-		if typeSpec.Name.Name == "" {
-			return nil, ErrStringEmpty
-		}
-		if _, ok := annotationToStructName[annotation]; ok {
-			return nil, ErrDuplicateAnnotation
-		}
-		annotationToStructName[annotation] = typeSpec.Name.Name
-	}
-	return annotationToStructName, nil
-}
-
-func generateFromAnnotationToStructName(goFile string, pkg string, annotationToStructName map[annotation]string) error {
-	enumTypeToEnumValues, err := getEnumTypeToEnumValues(annotationToStructName)
-	if err != nil {
-		return err
-	}
-	return generateFromEnumTypeToEnumValues(goFile, pkg, enumTypeToEnumValues)
-}
-
-func getEnumTypeToEnumValues(annotationToStructName map[annotation]string) (map[string][]*EnumValue, error) {
-	enumTypeToEnumValues := make(map[string][]*EnumValue)
-	for annotation, structName := range annotationToStructName {
-		if _, ok := enumTypeToEnumValues[annotation.enumType]; !ok {
-			enumTypeToEnumValues[annotation.enumType] = make([]*EnumValue, 0)
-		}
-		enumTypeToEnumValues[annotation.enumType] = append(
-			enumTypeToEnumValues[annotation.enumType],
+		enumTypeToEnumValues[enumType] = append(
+			enumTypeToEnumValues[enumType],
 			&EnumValue{
-				Name:       annotation.enumValue,
-				Id:         annotation.id,
-				StructName: structName,
+				Name:       enumValueName,
+				Id:         uint(enumValueId),
+				StructName: structDescriptor.Name,
 			},
 		)
 	}
@@ -392,7 +231,6 @@ func getEnumTypeToEnumValues(annotationToStructName map[annotation]string) (map[
 	return enumTypeToEnumValues, nil
 }
 
-// TODO(pedge)
 func validateEnumTypeToEnumValues(enumTypeToEnumValues map[string][]*EnumValue) error {
 	for _, enumValues := range enumTypeToEnumValues {
 		seenNames := make(map[string]bool)
@@ -414,10 +252,6 @@ func validateEnumTypeToEnumValues(enumTypeToEnumValues map[string][]*EnumValue) 
 		}
 	}
 	return nil
-}
-
-func generateFromEnumTypeToEnumValues(goFile string, pkg string, enumTypeToEnumValues map[string][]*EnumValue) error {
-	return generateFromGenData(goFile, getGenData(pkg, enumTypeToEnumValues))
 }
 
 func getGenData(pkg string, enumTypeToEnumValues map[string][]*EnumValue) *GenData {
@@ -445,40 +279,4 @@ func getEnumType(name string, enumValues []*EnumValue) *EnumType {
 		Name:       name,
 		EnumValues: enumValues,
 	}
-}
-
-func generateFromGenData(goFile string, genData *GenData) (retErr error) {
-	outputFile := "gen_enumtype_" + goFile
-	output, err := os.Create(outputFile)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := output.Close(); err != nil && retErr == nil {
-			retErr = err
-		}
-	}()
-	template := template.New("root")
-	template.Funcs(
-		map[string]interface{}{
-			"lowerCaseFirstLetter": lowerCaseFirstLetter,
-			"upperCaseFirstLetter": upperCaseFirstLetter,
-		},
-	)
-	if _, err := template.Parse(templateString()); err != nil {
-		return err
-	}
-	return template.Execute(output, genData)
-}
-
-func lowerCaseFirstLetter(s string) string {
-	a := []rune(s)
-	a[0] = unicode.ToLower(a[0])
-	return string(a)
-}
-
-func upperCaseFirstLetter(s string) string {
-	a := []rune(s)
-	a[0] = unicode.ToUpper(a[0])
-	return string(a)
 }
